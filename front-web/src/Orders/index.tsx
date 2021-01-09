1794141457ebaa151a37e7b6505d11d7610ead8a
JoaoPedroCAS <joaosa@alunos.utfpr.edu.br>
2021-01-09 16:37:27 -0300

    Final da 2 aula.

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
+import { Product } from './types';
 
 
 function Orders(){
-return(
-    <div className = "orders-container">
-        <StepsHeader />
-    </div>
-    )
+    const [products, setProducts] = useState<Product[]>([]);
+    useEffect(()=>{
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, [])
+    return(
+        <div className = "orders-container">
+            <StepsHeader />
+            <ProductsList  products ={products}/>
+        </div>
+        )
 }
 
 export default Orders;
