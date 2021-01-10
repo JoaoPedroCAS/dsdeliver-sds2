054c2914372d8a4cc3406525c727990e2b188373
JoaoPedroCAS <joaosa@alunos.utfpr.edu.br>
2021-01-10 12:06:11 -0300

    Finish Front Web

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -68,7 +68,11 @@ function OrderLocation({onChangeLocation}:Props) {
                     onChange={value => handleChangeSelect(value as Place)}
                     />
                 </div>
-                    <MapContainer center={address.position} zoom={17} key={address.position.lat} scrollWheelZoom={true}>
+                    <MapContainer 
+                    center={address.position} 
+                    zoom={17} 
+                    key={address.position.lat} 
+                    scrollWheelZoom={true}>
                         <TileLayer
                             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
